23d2f4128cb7199ddbf7f9ba251a7079bd675691
Vladimir Razuvaev <skyudaff@mail.ru>
2023-11-13 22:00:23 +0300

    added Integration test

--- src/test/java/com/example/springboot/ApplicationTests.java
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -1,14 +1,38 @@
 package com.example.springboot;
 
-import com.example.springboot.config.JavaConfig;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    private static final GenericContainer<?> devApp = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
 
+    @Container
+    private static final GenericContainer<?> prodApp = new GenericContainer<>("prodapp:1.0")
+            .withExposedPorts(8081);
+
+    @Test
+    void contextLoads() {
+        ResponseEntity<String> devEntity = restTemplate.getForEntity("http://localhost:" + devApp.getMappedPort(8080) + "/profile", String.class);
+        System.out.println("DEV: " + devEntity.getBody());
+        Assertions.assertEquals("Current profile is dev", devEntity.getBody());
+
+
+        ResponseEntity<String> prodEntity = restTemplate.getForEntity("http://localhost:" + prodApp.getMappedPort(8081) + "/profile", String.class);
+        System.out.println("PROD: " + prodEntity.getBody());
+        Assertions.assertEquals("Current profile is production", prodEntity.getBody());
+    }
 }
