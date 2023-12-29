a9f4787f9d0437ef552db6c590aff410d75d7230
Vladimir Razuvaev <skyudaff@mail.ru>
2023-12-29 19:35:32 +0300

    added Security methods

--- src/main/java/com/example/springboot/controller/ProfileController.java
+++ src/main/java/com/example/springboot/controller/ProfileController.java
@@ -1,7 +1,12 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.profile.SystemProfile;
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +26,28 @@ public class ProfileController {
     public String getProfile() {
         return profile.getProfile();
     }
-}
 
+    @Secured("ROLE_READ")
+    @GetMapping("/secured")
+    public String getSecuredRole() {
+        return "READ ONLY";
+    }
+
+    @RolesAllowed("WRITE")
+    @GetMapping("/roles-allow")
+    public String getRolesAllow() {
+        return "WRITE ONLY";
+    }
+
+    @PostAuthorize("hasAnyAuthority('ROLE_WRITE','ROLE_DELETE')")
+    @GetMapping("/authorize")
+    public String getPreAuthorize() {
+        return "WRITE & DELETE";
+    }
+
+    @PreAuthorize("#username == authentication.principal.username")
+    @GetMapping("/user")
+    public String getUsername(@RequestParam String username) {
+        return String.format("Username \"%s\" is correct", username);
+    }
+}
